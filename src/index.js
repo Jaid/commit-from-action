b3b30228fac026092ce08f0bd3c26e1a3df42f4f
Jaid <jaid.jsx@gmail.com>
2021-05-24 04:15:54 +0200

    Improved exec call readability
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -153,7 +153,7 @@ export default class CommitManager {
       return
     }
     this.githubToken = getInput(this.options.githubTokenInputName, {required: true})
-    await exec("git", ["push", "-f", `https://${process.env.GITHUB_ACTOR}:${this.githubToken}@github.com/${process.env.GITHUB_REPOSITORY}.git`, `HEAD:${this.branch}`])
+    await exec("git", ["push", "--force", `https://${process.env.GITHUB_ACTOR}:${this.githubToken}@github.com/${process.env.GITHUB_REPOSITORY}.git`, `HEAD:${this.branch}`])
     const octokit = new GitHub(this.githubToken)
     const pullRequest
       = (await octokit.pulls.list({
